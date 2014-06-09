f5d816b701dfdec7edad769ed5db236dc5808d64
Shiyao Qi <qishiyao2008@126.com>
2014-06-09 19:51:53 +0800

    Add type_alias project.

---
+++ type_alias/type_alias/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  type_alias
+//
+//  Created by Jack on 6/8/14.
+//  Copyright (c) 2014 Jack. All rights reserved.
+//
+
+import Foundation
+
+// 与C语言中的typedef类似
+
+typealias ID = UInt32
+
+var max = ID.max
+println(max)
+
+var id:ID = 20
+println(id)
+
