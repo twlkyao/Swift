5a0e65db845ef4aa573d75a0c9e6c943a4d1cf61
Shiyao Qi <qishiyao2008@126.com>
2014-06-09 19:53:10 +0800

    Add var_let project.

---
+++ var_let/var_let/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  var_let
+//
+//  Created by Jack on 6/8/14.
+//  Copyright (c) 2014 Jack. All rights reserved.
+//
+
+import Foundation
+
+let product_constant = "iPhone6"
+var product_var = "iPad5"
+
+var id = 50
+
+println(id)
+
+var x1 = 30, x2 = "abc"
+
+var value:Int = 33
+
+var 齐士垚 = "Shiyao Qi"
+
+println(齐士垚)
+
+println("x1=\(x1),齐士垚=\(齐士垚)")
